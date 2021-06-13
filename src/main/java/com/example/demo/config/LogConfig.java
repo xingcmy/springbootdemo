29021004729942b05042123dab1b16f8da979e9a
xingcmy <67186152+xingcmy@users.noreply.github.com>
2021-06-13 13:54:05 +0800

    xingcmy

---
+++ src/main/java/com/example/demo/config/LogConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class LogConfig {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LogConfig.class);
+
+}
